fa7066fb8a0e75a87d13978283b4a9a51da78d37
Tanmay Kadam <Tanmay.kdm@gmail.com>
2022-05-20 17:40:45 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,20 +2,37 @@ provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "demo" {
-  name = "terraf"
-  location = "eastus"
+resource "azurerm_resource_group" "example" {
+  name     = "tf-resources"
+  location = var.location
 }
 
-resource "azurerm_storage_account" "example" {
-  name                     = "storageacct250998"
-  resource_group_name      = azurerm_resource_group.demo.name
-  location                 = azurerm_resource_group.demo.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
+resource "azurerm_sql_server" "example" {
+  name                         = "tfdb2509-sqlsvr"
+  resource_group_name          = azurerm_resource_group.example.name
+  location                     = azurerm_resource_group.example.location
+  version                      = "12.0"
+  administrator_login          = "4dm1n157r470r"
+  administrator_login_password = "4-v3ry-53cr37-p455w0rd"
+}
 
-  network_rules {
-    default_action = "Deny"
-    ip_rules       = ["23.45.1.0/30"]
-  }
+resource "azurerm_sql_database" "example" {
+  name                             = "tfdb2509-db"
+  resource_group_name              = azurerm_resource_group.example.name
+  location                         = azurerm_resource_group.example.location
+  server_name                      = azurerm_sql_server.example.name
+  edition                          = "Basic"
+  collation                        = "SQL_Latin1_General_CP1_CI_AS"
+  create_mode                      = "Default"
+  requested_service_objective_name = "Basic"
+}
+
+# Enables the "Allow Access to Azure services" box as described in the API docs
+# https://docs.microsoft.com/en-us/rest/api/sql/firewallrules/createorupdate
+resource "azurerm_sql_firewall_rule" "example" {
+  name                = "allow-azure-services"
+  resource_group_name = azurerm_resource_group.example.name
+  server_name         = azurerm_sql_server.example.name
+  start_ip_address    = "0.0.0.0"
+  end_ip_address      = "0.0.0.0"
 }
