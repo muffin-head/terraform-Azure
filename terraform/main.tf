e051a2482eb3460c7f5c6b4142223d2af970f245
Tanmay Kadam <Tanmay.kdm@gmail.com>
2022-05-20 17:53:41 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,20 +2,251 @@ provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "demo" {
-  name = "terraf"
+resource "azurerm_resource_group" "example" {
+  name     = "datafac-resource"
   location = "eastus"
 }
 
-resource "azurerm_storage_account" "example" {
-  name                     = "storageacct250998"
-  resource_group_name      = azurerm_resource_group.demo.name
-  location                 = azurerm_resource_group.demo.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
+resource "azurerm_virtual_network" "example" {
+  name                = "virtaulnet2509-VN"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+}
 
-  network_rules {
-    default_action = "Deny"
-    ip_rules       = ["23.45.1.0/30"]
+resource "azurerm_subnet" "example" {
+  name                 = "virtualsubnet2509-SN"
+  resource_group_name  = azurerm_resource_group.example.name
+  virtual_network_name = azurerm_virtual_network.example.name
+  address_prefixes     = ["10.0.0.0/24"]
+}
+
+resource "azurerm_subnet_network_security_group_association" "example" {
+  subnet_id                 = azurerm_subnet.example.id
+  network_security_group_id = azurerm_network_security_group.example.id
+}
+
+resource "azurerm_public_ip" "vm" {
+  name                = "virtualip2509-PIP"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  allocation_method   = "Dynamic"
+}
+
+resource "azurerm_network_security_group" "example" {
+  name                = "virtualsecgroup2509-NSG"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+}
+
+resource "azurerm_network_security_rule" "RDPRule" {
+  name                        = "RDPRule"
+  resource_group_name         = azurerm_resource_group.example.name
+  priority                    = 1000
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = 3389
+  source_address_prefix       = "167.220.255.0/25"
+  destination_address_prefix  = "*"
+  network_security_group_name = azurerm_network_security_group.example.name
+}
+
+resource "azurerm_network_security_rule" "MSSQLRule" {
+  name                        = "MSSQLRule"
+  resource_group_name         = azurerm_resource_group.example.name
+  priority                    = 1001
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = 1433
+  source_address_prefix       = "167.220.255.0/25"
+  destination_address_prefix  = "*"
+  network_security_group_name = azurerm_network_security_group.example.name
+}
+
+resource "azurerm_network_interface" "example" {
+  name                = "netinterface2509-NIC"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+
+  ip_configuration {
+    name                          = "exampleconfiguration1"
+    subnet_id                     = azurerm_subnet.example.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.vm.id
   }
+}
+
+resource "azurerm_network_interface_security_group_association" "example" {
+  network_interface_id      = azurerm_network_interface.example.id
+  network_security_group_id = azurerm_network_security_group.example.id
+}
+
+resource "azurerm_virtual_machine" "example" {
+  name                  = "vm2509-VM"
+  location              = azurerm_resource_group.example.location
+  resource_group_name   = azurerm_resource_group.example.name
+  network_interface_ids = [azurerm_network_interface.example.id]
+  vm_size               = "Standard_DS14_v2"
+
+  storage_image_reference {
+    publisher = "MicrosoftSQLServer"
+    offer     = "SQL2017-WS2016"
+    sku       = "SQLDEV"
+    version   = "laexample"
+  }
+
+  storage_os_disk {
+    name              = "vmosdisk-OSDisk"
+    caching           = "ReadOnly"
+    create_option     = "FromImage"
+    managed_disk_type = "Premium_LRS"
+  }
+
+  os_profile {
+    computer_name  = "winhost01"
+    admin_username = "exampleadmin"
+    admin_password = "Password1234!"
+  }
+
+  os_profile_windows_config {
+    timezone                  = "Pacific Standard Time"
+    provision_vm_agent        = true
+    enable_automatic_upgrades = true
+  }
+}
+
+resource "azurerm_mssql_virtual_machine" "example" {
+  virtual_machine_id = azurerm_virtual_machine.example.id
+  sql_license_type   = "PAYG"
+}
+
+resource "azurerm_virtual_network" "test" {
+  name                = "viryaulnet2509-VN"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+}
+
+resource "azurerm_subnet" "test" {
+  name                 = "internal"
+  resource_group_name  = azurerm_resource_group.example.name
+  virtual_network_name = azurerm_virtual_network.test.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+
+resource "azurerm_public_ip" "test" {
+  name                = "$publicip2509-PIP"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  allocation_method   = "Dynamic"
+}
+
+resource "azurerm_network_interface" "test" {
+  name                = "aznetwrokinterfcae2509-INT"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.test.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.test.id
+  }
+}
+
+resource "azurerm_virtual_machine" "test" {
+  name                  = "azvm2509-VM"
+  location              = azurerm_resource_group.example.location
+  resource_group_name   = azurerm_resource_group.example.name
+  network_interface_ids = [azurerm_network_interface.test.id]
+  vm_size               = "Standard_F4"
+
+  storage_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2016-Datacenter"
+    version   = "latest"
+  }
+
+  storage_os_disk {
+    name              = "myosdisk1"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
+  }
+
+  os_profile {
+    computer_name  = "azureprof-VM"
+    admin_username = "testadmin"
+    admin_password = "Password1234!"
+  }
+
+  os_profile_windows_config {
+    timezone           = "Pacific Standard Time"
+    provision_vm_agent = true
+  }
+}
+
+resource "azurerm_virtual_machine_extension" "test" {
+  name                 = "azvirext2509-EXT"
+  virtual_machine_id   = azurerm_virtual_machine.test.id
+  publisher            = "Microsoft.Compute"
+  type                 = "CustomScriptExtension"
+  type_handler_version = "1.10"
+  settings = jsonencode({
+    "fileUris"         = ["https://raw.githubusercontent.com/Azure/azure-quickstart-templates/00b79d2102c88b56502a63041936ef4dd62cf725/101-vms-with-selfhost-integration-runtime/gatewayInstall.ps1"],
+    "commandToExecute" = "powershell -ExecutionPolicy Unrestricted -File gatewayInstall.ps1 ${azurerm_data_factory_integration_runtime_self_hosted.host.primary_authorization_key} && timeout /t 120"
+  })
+}
+
+resource "azurerm_resource_group" "host" {
+  name     = "reso2509-resources"
+  location = "eastus"
+}
+
+resource "azurerm_data_factory" "host" {
+  name                = "azdf2509DFHOST"
+  location            = azurerm_resource_group.host.location
+  resource_group_name = azurerm_resource_group.host.name
+}
+
+resource "azurerm_data_factory_integration_runtime_self_hosted" "host" {
+  name            = "runtime2509IRHOST"
+  data_factory_id = azurerm_data_factory.host.id
+}
+
+resource "azurerm_resource_group" "target" {
+  name     = "azres250998-resources"
+  location = "eastus"
+}
+
+resource "azurerm_role_assignment" "target" {
+  scope                = azurerm_data_factory.host.id
+  role_definition_name = "Contributor"
+  principal_id         = azurerm_data_factory.target.identity[0].principal_id
+}
+
+resource "azurerm_data_factory" "target" {
+  name                = "tardf2509DFTGT"
+  location            = azurerm_resource_group.target.location
+  resource_group_name = azurerm_resource_group.target.name
+
+  identity {
+    type = "SystemAssigned"
+  }
+}
+
+resource "azurerm_data_factory_integration_runtime_self_hosted" "target" {
+  name            = "intruntime250998IRTGT"
+  data_factory_id = azurerm_data_factory.target.id
+
+  rbac_authorization {
+    resource_id = azurerm_data_factory_integration_runtime_self_hosted.host.id
+  }
+
+  depends_on = [azurerm_role_assignment.target, azurerm_virtual_machine_extension.test]
 }
