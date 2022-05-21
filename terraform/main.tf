100e998b9ce3a4cdab80157fdb455678b81a2d96
Tanmay Kadam <Tanmay.kdm@gmail.com>
2022-05-22 00:56:01 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -25,22 +25,10 @@ resource "azurerm_storage_container" "example" {
   storage_account_name  = azurerm_storage_account.example.name
   container_access_type = "blob"
 }
-
-resource "azurerm_storage_account" "example2" {
-  name                = "terraform25storageacct2"
-  resource_group_name = azurerm_resource_group.example.name
-  location            = azurerm_resource_group.example.location
-
-  account_tier                    = "Standard"
-  account_kind                    = "StorageV2"
-  account_replication_type        = "LRS"
-  enable_https_traffic_only       = true
-  access_tier                     = "Hot"
-  allow_nested_items_to_be_public = true
-}
-
-resource "azurerm_storage_container" "example2" {
-  name                  = "terraform25storagecontainer2"
-  storage_account_name  = azurerm_storage_account.example2.name
-  container_access_type = "blob"
+resource "azurerm_storage_blob" "example" {
+  name                   = "moviesDB.csv"
+  storage_account_name   = azurerm_storage_account.example.name
+  storage_container_name = azurerm_storage_container.example.name
+  type                   = "Block"
+  source                 = "moviesDB.csv"
 }
