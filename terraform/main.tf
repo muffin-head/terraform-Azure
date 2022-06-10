bede13c4f04efbb607e27ac23dd86a68c75f281b
Tanmay Kadam <Tanmay.kdm@gmail.com>
2022-06-10 21:30:02 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -75,7 +75,7 @@ resource "databricks_cluster" "shared_autoscaling" {
 }
 
 resource "databricks_notebook" "notebook" {
-  content = base64encode("print('Welcome to your Python notebook')")
+  content = "/1.2 Databricks Platform.ipynb"
   path = var.notebook_path
   overwrite = false
   mkdirs = true
